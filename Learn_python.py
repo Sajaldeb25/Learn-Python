85ccd7fc7ecfbbf095e9031b221fb81eaedbfa99
sajaldebnath02@gmail.com <sajaldebnath02@gmail.com>
2019-08-26 22:46:36 +0600

    After exercise 1 (Factorial)

--- Learn_python.py
+++ Learn_python.py
@@ -25,12 +25,106 @@ sqrt(9) # do the same job
 
 
 ## printing the same graph using loop and list 
-ts_length = 100
+loop_length = 100
 values = []   # Empty list
 
-for i in range(ts_length):
+for i in range(loop_length):
     e = np.random.randn()
     values.append(e)
 
 plt.plot(values)
 plt.show()
+
+
+
+##explain list
+x = ['Sajal',22, 'cse']
+x.append(50000)
+x.pop()
+x.append(60000)
+
+
+## printing the same graph using while loop
+n = 100
+value = []
+i = 0
+while i<n:
+    e = np.random.randn()
+    value.append(e)
+    i = i+1
+
+plt.plot(value)
+plt.show()
+
+
+## printing the same graph using while loop
+def generate_data(n):
+    value = []
+    i = 0
+    while i<n:
+        e = np.random.randn()
+        value.append(e)
+        i = i+1
+        
+data = generate_data(100)
+plt.plot(value)
+plt.show()
+
+
+## printing the same graph using conndition
+def generate_data(n,g_type):
+    value = []
+    i = 0
+    while i<n :
+        if g_type == 'A':
+            e = np.random.uniform(4, 5)
+        else:
+            e = np.random.randn()
+        value.append(e)
+        i = i+1
+    return value
+
+data = generate_data(100, 'A')
+plt.plot(data)
+plt.show()
+
+
+## same graph 
+def generate_data(n, generator_type):
+    e_values = []
+    for i in range(n):
+        e = generator_type()
+        e_values.append(e)
+    return e_values
+
+data = generate_data(100, np.random.uniform)
+plt.plot(data)
+plt.show()
+
+
+## List 
+animals = ['dog', 'cat', 'bird']
+plurals = [animal + 's' for animal in animals]
+plurals
+
+
+doubles = [2 * x for x in range(11)]
+doubles
+##--------------List End--------------------
+
+##Excersise 1 (Factorial )
+ 
+def factorial(n):
+    k = 1
+    for i in range(n):
+        k = k*(i+1)
+    return k
+
+factorial(5)
+
+
+
+
+
+
+
