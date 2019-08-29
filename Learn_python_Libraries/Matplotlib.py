5f1fa2e416af35433112c7b97a3f5c3c524816e7
sajaldebnath02@gmail.com <sajaldebnath02@gmail.com>
2019-08-30 01:26:51 +0600

    Plot practise more

--- Learn_python_Libraries/Matplotlib.py
+++ Learn_python_Libraries/Matplotlib.py
@@ -50,3 +50,21 @@ ax.legend(loc='upper center')
 ax.set_yticks([-1,-.5, 0, .5, 1])
 plt.title('Test plot')
 plt.show()
+
+
+
+##More plot on one axis
+
+from scipy.stats import norm
+from random import uniform
+
+fig, ax = plt.subplots()
+x = np.linspace(-4, 4, 150)
+for i in range(3):
+    print(i)
+    m, s = uniform(-1, 1), uniform(1, 2)
+    y = norm.pdf(x, loc=m, scale=s)
+    current_label = '$\mu = {m:.2}$'
+    ax.plot(x, y, linewidth=2, alpha=0.6, label=current_label)
+ax.legend()
+plt.show()
