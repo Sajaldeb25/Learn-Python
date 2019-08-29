6d3a11c066c9bd8551153fc72072b954b915cecb
sajaldebnath02@gmail.com <sajaldebnath02@gmail.com>
2019-08-30 00:58:19 +0600

    exercise 3 solved

--- Learn_python_Libraries/NumPy.py
+++ Learn_python_Libraries/NumPy.py
@@ -5,3 +5,80 @@ Created on Fri Aug 30 00:36:42 2019
 @author: Ryans
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""
+Modifies ecdf.py from QuantEcon to add in a plot method
+
+"""
+
+class ECDF:
+    """
+    One-dimensional empirical distribution function given a vector of
+    observations.
+
+    Parameters
+    ----------
+    observations : array_like
+        An array of observations
+
+    Attributes
+    ----------
+    observations : array_like
+        An array of observations
+
+    """
+
+    def __init__(self, observations):
+        self.observations = np.asarray(observations)
+
+    def __call__(self, x):
+        """
+        Evaluates the ecdf at x
+
+        Parameters
+        ----------
+        x : scalar(float)
+            The x at which the ecdf is evaluated
+
+        Returns
+        -------
+        scalar(float)
+            Fraction of the sample less than x
+
+        """
+        return np.mean(self.observations <= x)
+
+    def plot(self, a=None, b=None):
+        """
+        Plot the ecdf on the interval [a, b].
+
+        Parameters
+        ----------
+        a : scalar(float), optional(default=None)
+            Lower endpoint of the plot interval
+        b : scalar(float), optional(default=None)
+            Upper endpoint of the plot interval
+
+        """
+
+        # === choose reasonable interval if [a, b] not specified === #
+        if a is None:
+            a = self.observations.min() - self.observations.std()
+        if b is None:
+            b = self.observations.max() + self.observations.std()
+
+        # === generate plot === #
+        x_vals = np.linspace(a, b, num=100)
+        f = np.vectorize(self.__call__)
+        plt.plot(x_vals, f(x_vals))
+        plt.show()
+        
+        
+        
+        
+        
+X = np.random.randn(1000)
+F = ECDF(X)
+F.plot()
