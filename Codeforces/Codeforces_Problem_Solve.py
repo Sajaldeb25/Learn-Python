887519984f804f5c84ed2a327102d3cd7340abb9
sajaldebnath02@gmail.com <sajaldebnath02@gmail.com>
2019-08-30 09:14:44 +0600

    1A not solved

--- Codeforces/Codeforces_Problem_Solve.py
+++ Codeforces/Codeforces_Problem_Solve.py
@@ -4,6 +4,7 @@ Created on Fri Aug 30 07:58:36 2019
 
 @author: Ryans
 """
+import numpy as np
 ## 4A - Watermelon
 x = int(input())
 #print(x)
@@ -11,3 +12,20 @@ if x%2 == 0 and x>=4:
     print("YES")
 else:
     print("NO")
+
+
+## 1A
+m,n,a = map(int,raw_input().split() )
+print(m)
+print(n)
+print(a)
+print-n/a
+print(-m/a)
+
+print(-5/2)  # making ceil 
+
+    
+print(block*block1)
+
+n,m,a=map(int,raw_input().split())
+print-n/a*(-m/a)
