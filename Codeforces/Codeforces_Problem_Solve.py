566da5d911f889386fad22733b5933d9d809e8ce
sajaldebnath02@gmail.com <sajaldebnath02@gmail.com>
2019-08-30 08:08:16 +0600

    4A solved

---
+++ Codeforces/Codeforces_Problem_Solve.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 30 07:58:36 2019
+
+@author: Ryans
+"""
+## 4A - Watermelon
+x = int(input())
+#print(x)
+if x%2 == 0 and x>=4:
+    print("YES")
+else:
+    print("NO")
